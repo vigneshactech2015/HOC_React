6780c627db3637f0027f7e3b20c195b708e4c48a
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2023-04-28 10:15:35 +0530

    Update App.js

--- HOC/App.js
+++ HOC/App.js
@@ -1,16 +1,10 @@
-import HOC from './HOC';
-
-//we get value as prop
-
-const App=({value})=>{
-    return(
-        <div>
-        <h1>This is {value.name}</h1>
-        <h1>I work as a {value.role}</h1>
-        <h1>I work for {value.company}</h1>
-        </div>
-    )
+import Comp1 from "./Comp1"
+export default function App(){
+    
+ return(
+ <div>
+     <Comp1 someprop="something"/>
+     </div>
+ )   
+    
 }
-
-export default HOC(App);
-//wrapping our component within our HOC.
